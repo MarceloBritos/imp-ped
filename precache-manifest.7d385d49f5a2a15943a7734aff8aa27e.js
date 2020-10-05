60765a6ae57a62bb2a7cc30eb63f425dfa2c4995
MarceloBritos <marcelobd07@gmail.com>
2020-10-05 02:48:14 -0300

    Updates

---
+++ precache-manifest.7d385d49f5a2a15943a7734aff8aa27e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc0b5721953077d12bb1ad43362b457",
+    "url": "/MarceloBritos/imp-ped/index.html"
+  },
+  {
+    "revision": "1df64fb2d945fa7a485b",
+    "url": "/MarceloBritos/imp-ped/static/css/main.a873e89f.chunk.css"
+  },
+  {
+    "revision": "d95506154e6929931133",
+    "url": "/MarceloBritos/imp-ped/static/js/2.0cb0b10a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MarceloBritos/imp-ped/static/js/2.0cb0b10a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df64fb2d945fa7a485b",
+    "url": "/MarceloBritos/imp-ped/static/js/main.48f5bfb0.chunk.js"
+  },
+  {
+    "revision": "cf0880d9488bb2b6cbea",
+    "url": "/MarceloBritos/imp-ped/static/js/runtime-main.0d13d46f.js"
+  },
+  {
+    "revision": "20bb958eac436ffc62cd7febcf3b71d9",
+    "url": "/MarceloBritos/imp-ped/static/media/default.20bb958e.jpg"
+  }
+]);
